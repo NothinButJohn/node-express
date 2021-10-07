79772f4f7cff5b72c3ba56eb5109347068abd2b3
john harkins <johnleoharkins3@gmail.com>
2021-10-07 10:29:32 -0500

    init

---
+++ database/database.ts
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+
+// // Option 1: Passing a connection URI
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+//
+//
+// // Option 2: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//     host: 'localhost',
+//     dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+// });
+module.exports = {};
